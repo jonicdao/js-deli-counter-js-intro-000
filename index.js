4743e61d0ed686a4f04e19faa0594dd766474060
Jose Nicdao <jonicdao@gmail.com>
2019-08-18 23:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,10 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  
+  while (katzDeliLine.length > 0) {
+    var array = []
+    for (var i = 0; i < katsDeliLine.length; i++)
+    array.push(' ${i+1}. ${line[1]}')
+  }
+    return "The line is currently empty."
 }
