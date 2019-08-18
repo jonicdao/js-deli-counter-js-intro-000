49774978381ed353f81cf1cbe1667a20f8d8717a
Jose Nicdao <jonicdao@gmail.com>
2019-08-18 23:10:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function takeANumber(katzDeliLine, name) {
   return ('Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.')
 } 
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    var nowServing = katzDeliLine[0]
-    katzDeliLine.shift()
-    return 'Currently serving ' + nowServing + '.'
-  }
-  else {
+// function nowServing(katzDeliLine) {
+//   if (katzDeliLine.length > 0) {
+//     var nowServing = katzDeliLine[0]
+//     katzDeliLine.shift()
+//     return 'Currently serving ' + nowServing + '.'
+//   }
+//   else {
   return "There is nobody waiting to be served!"
   }
 }
