a080604fa34c1ecd105648e135d822d2b93fdc08
Jose Nicdao <jonicdao@gmail.com>
2019-08-18 22:16:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   let i =0
   while (i < katzDeliLine.length) {
-    return katzDeliLine[i]
+    return 'Currently serving ' + katzDeliLine[i].name
     katzDeliLine.shift()
     i++
   }
