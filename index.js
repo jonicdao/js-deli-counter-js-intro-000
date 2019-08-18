d4282f8df522e9564aa8406e7490031cb91199e5
Jose Nicdao <jonicdao@gmail.com>
2019-08-18 23:11:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ function takeANumber(katzDeliLine, name) {
   return ('Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.')
 } 
 
-// function nowServing(katzDeliLine) {
-//   if (katzDeliLine.length > 0) {
-//     var nowServing = katzDeliLine[0]
-//     katzDeliLine.shift()
-//     return 'Currently serving ' + nowServing + '.'
-//   }
-//   else {
-//   return "There is nobody waiting to be served!"
-//   }
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var nowServing = katzDeliLine[0]
+    katzDeliLine.shift()
+    return 'Currently serving ' + nowServing + '.'
+  }
+  else {
+  return "There is nobody waiting to be served!"
+  }
 }
 
 function currentLine(katzDeliLine) {
   while (katzDeliLine.length > 0) {
     var array = []
     for (var i = 0; i < katzDeliLine.length; i++)
-    array.push( '${i + 1}.  ${line[i]}')
+    array.push( '${i + 1}.  ${line[i]})
     return "The Line is currently: " + array
   }
     return "The line is currently empty."
